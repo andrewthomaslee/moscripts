d5493e47433f6f76f09c675b7f218894eef2e783
Andrew Thomas Lee <andrewthomaslee.business@gmail.com>
2025-08-13 15:37:31 -0500

    _

--- flake.nix
+++ flake.nix
@@ -49,47 +49,78 @@
       inherit python;
     };
 
+    pytestOverlay = final: prev: {
+      moscripts = prev.moscripts.overrideAttrs (old: {
+        passthru =
+          (old.passthru or {})
+          // {
+            tests = let
+              virtualenv = final.mkVirtualEnv "moscripts-pytest-env" {
+                moscripts = ["dev"];
+              };
+            in
+              (old.tests or {})
+              // {
+                pytest = pkgs.stdenv.mkDerivation {
+                  name = "${final.moscripts.name}-pytest";
+                  inherit (final.moscripts) src;
+                  nativeBuildInputs = [virtualenv];
+                  dontConfigure = true;
+                  buildPhase = ''
+                    runHook preBuild
+                    pytest --junit-xml=pytest.xml
+                    runHook postBuild
+                  '';
+                  installPhase = ''
+                    runHook preInstall
+                    mv pytest.xml $out
+                    runHook postInstall
+                  '';
+                };
+              };
+          };
+      });
+    };
+
     pythonSet = baseSet.overrideScope (
       lib.composeManyExtensions [
         pyproject-build-systems.overlays.default
         overlay
+        pytestOverlay
+      ]
+    );
+
+    venv = pythonSet.mkVirtualEnv "moscripts-default-env" workspace.deps.default;
+    # Dev shell helpers
+    editableOverlay = workspace.mkEditablePyprojectOverlay {
+      root = "$REPO_ROOT";
+    };
+
+    editablePythonSet = pythonSet.overrideScope (
+      lib.composeManyExtensions [
+        editableOverlay
         (final: prev: {
           moscripts = prev.moscripts.overrideAttrs (old: {
-            passthru =
-              (old.passthru or {})
-              // {
-                tests = let
-                  virtualenv = final.mkVirtualEnv "moscripts-pytest-env" {
-                    moscripts = ["dev"];
-                  };
-                in
-                  (old.tests or {})
-                  // {
-                    pytest = pkgs.stdenv.mkDerivation {
-                      name = "${final.moscripts.name}-pytest";
-                      inherit (final.moscripts) src;
-                      nativeBuildInputs = [virtualenv];
-                      dontConfigure = true;
-                      buildPhase = ''
-                        runHook preBuild
-                        pytest --junit-xml=pytest.xml
-                        runHook postBuild
-                      '';
-                      installPhase = ''
-                        runHook preInstall
-                        mv pytest.xml $out
-                        runHook postInstall
-                      '';
-                    };
-                  };
+            src = lib.fileset.toSource {
+              root = old.src;
+              fileset = lib.fileset.unions [
+                (old.src + "/pyproject.toml")
+                (old.src + "/README.md")
+                (old.src + "/src/moscripts/__init__.py")
+              ];
+            };
+            nativeBuildInputs =
+              old.nativeBuildInputs
+              ++ final.resolveBuildSystem {
+                editables = [];
               };
           });
         })
       ]
     );
 
-    venv = pythonSet.mkVirtualEnv "moscripts-default-env" workspace.deps.default;
-
+    virtualenvDev = editablePythonSet.mkVirtualEnv "moscripts-dev-env" workspace.deps.all;
+    # ------------------------------------------------------------------------------ #
     # App discovery and creation
     appsBasedir = ./apps;
     appFiles = filterAttrs (name: type: type == "regular" && hasSuffix ".py" name) (
@@ -132,36 +163,6 @@
         buildCommand = makeExecutable appName;
         buildInputs = [venv];
       };
-
-    # Dev shell helpers
-    editableOverlay = workspace.mkEditablePyprojectOverlay {
-      root = "$REPO_ROOT";
-    };
-
-    editablePythonSet = pythonSet.overrideScope (
-      lib.composeManyExtensions [
-        editableOverlay
-        (final: prev: {
-          moscripts = prev.moscripts.overrideAttrs (old: {
-            src = lib.fileset.toSource {
-              root = old.src;
-              fileset = lib.fileset.unions [
-                (old.src + "/pyproject.toml")
-                (old.src + "/README.md")
-                (old.src + "/src/moscripts/__init__.py")
-              ];
-            };
-            nativeBuildInputs =
-              old.nativeBuildInputs
-              ++ final.resolveBuildSystem {
-                editables = [];
-              };
-          });
-        })
-      ]
-    );
-
-    virtualenvDev = editablePythonSet.mkVirtualEnv "moscripts-dev-env" workspace.deps.all;
   in {
     # Create individual packages for each app and their container variants
     packages.x86_64-linux = let
